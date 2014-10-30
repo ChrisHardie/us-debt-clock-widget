58783efbee82e3f969fae0e4fc051ead973e450f
Chris Hardie <chris@summersault.com>
2014-10-30 09:09:10 -0400

    Initial unit testing framework

---
+++ tests/test-us-debtclock-widget.php
@@ -0,0 +1,12 @@
+<?php
+
+class DebtclockTest extends WP_UnitTestCase {
+
+	// testing framework is ready for expansion :)
+	// for now, we just make sure the universe still exists
+	public function testTruth() {
+
+		$this->assertTrue( true );
+	}
+}
+

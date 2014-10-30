45e7174e38a0b38de9ee4e0d8515240755fd4b14
Chris Hardie <chris@summersault.com>
2014-10-30 08:51:36 -0400

    Documentation, formatting, defense against source outages, better CSS management

--- us-debtclock-widget.php
+++ us-debtclock-widget.php
@@ -56,64 +56,84 @@ class Debtclock_Widget extends WP_Widget {
 		// If we can't get a value for the current debt info, at least display something.
 		if ( false === ( $debt_info = JCH_Debtclock::get_debt() ) ) {
 			$debt_amount = 'UNAVAILABLE';
-		} else {
+			$debt_amount_formatted = 'UNAVAILABLE';
+		} elseif ( is_numeric( $debt_info['close_today'] ) ) {
+
 			// The value from treasury.io is in millions, e.g. 1000 equals $1 billion.
 			$debt_amount = $debt_info['close_today'] * 1000;
-		}
 
-		// For maximum effect let's display the big number, with commas, no cents.
-		$debt_amount_formatted = number_format( $debt_amount, 0, '.', ',');
+			// For maximum effect let's display the big number, with commas, no cents.
+			$debt_amount_formatted = number_format( $debt_amount, 0, '.', ',');
 
-		wp_enqueue_style( 'debtclock_widget_style', plugins_url('style.css', __FILE__) );
+			if ( $instance['animate_p'] ) {
 
-		if ( $instance['animate_p'] ) {
+				// Calculate how much the debt increased per second on average
+				$debt_delta = ( ( ( $debt_info['close_today'] - $debt_info['open_today'] ) * 1000 ) / 86400 );
 
-			// Calculate how much the debt increased per second on average
-			$debt_delta = ( ( ( $debt_info['close_today'] - $debt_info['open_today'] ) * 1000 ) / 86400 );
+				wp_enqueue_script('jquery');
 
-			wp_enqueue_script('jquery');
+			}
 
+		} else {
+			$debt_amount = 'INVALID';
+			$debt_amount_formatted = 'INVALID';
 		}
 
 		// Output the widget content
 		echo $before_widget;
+
+		// Title
 		if ( !empty( $title ) ) {
 			echo $before_title . $title . $after_title;
 		}
-		echo '<p>' . esc_html_e( $instance['introduction'] ) . '</p>';
-		echo '<p><div id="debtclock_amount" class="us_debtclock_widget_amount" align="center">$'
-		     . esc_html( $debt_amount_formatted ) . '</div></p>';
 
-		if ( $instance['animate_p'] ) {
+		// User-defined introduction
+		if ( !empty( $instance['introduction'] ) ) {
+			echo '<div class="us_debtclock_widget_introduction">';
+			echo esc_html_e( $instance['introduction'] );
+			echo '</div>';
+		}
 
+		// Actual debt amount, formatted (maybe replaced by JS actions below)
+		echo '<div id="debtclock_amount" class="us_debtclock_widget_amount">$'
+		     . esc_html( $debt_amount_formatted ) . '</div>';
+
+		// If they want moving numbers and we're starting with a real number...
+		if ( $instance['animate_p'] && is_numeric( $debt_amount ) ) {
+
+			// Increment the amount by the per-second delta we calculated earlier
 			echo "<script type='text/javascript'>";
 			echo "
 					var INTERVAL = 1; // refresh interval in seconds
-				    var INCREMENT = " . esc_js( $debt_delta ) . ";  // increase per tick
-				    var START_VALUE = " . esc_js( $debt_amount ) . "; // initial value when it's the start date
-				    var count = 0;
+					var INCREMENT = " . esc_js( $debt_delta ) . ";  // increase per tick
+					var START_VALUE = " . esc_js( $debt_amount ) . "; // initial value when it's the start date
+					var count = 0;
 
-				    jQuery(document).ready(function() {
+					jQuery(document).ready(function() {
 
-				        var msInterval = INTERVAL * 1000;
+						var msInterval = INTERVAL * 1000;
 						var now = new Date();
 						count = START_VALUE;
 
 						window.setInterval( function(){
 
-				            count += INCREMENT;
+							count += INCREMENT;
 							count_formatted = count.toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, \"$1,\");
 							jQuery('#debtclock_amount').html(\"$\" + count_formatted);
 
-				        }, msInterval);
+						}, msInterval);
 
-				    });
+					});
 				";
 			echo "</script>";
 		}
 
+		// Give credit where it's due?
 		if ( $instance['show_credit_p'] ) {
-			echo '<p class="us_debtclock_widget_credit"><a target="_blank" href=" ' . esc_html( $debt_info['url'] ) . '">Source</a>, via <a class="us_debtclock_widget_credit_link" href="http://treasury.io/">treasury.io</a></p>';
+			echo '<p class="us_debtclock_widget_credit">
+				<a class="us_debtclock_widget_credit_link" target="_blank" href=" ' . esc_html( $debt_info['url'] ) . '">Source</a>,
+				via <a class="us_debtclock_widget_credit_link" target="_blank" href="http://treasury.io/">treasury.io</a>
+			</p>';
 		}
 
 		echo $after_widget;
@@ -192,7 +212,7 @@ class Debtclock_Widget extends WP_Widget {
 
 class JCH_Debtclock {
 	function __construct() {
-		add_action( 'init', array( $this, 'init'), 1 );
+		add_action( 'init', array( $this, 'init' ), 1 );
 
 		register_activation_hook( __FILE__, array( $this, 'us_debtclock_widget_activation' ) );
 		register_deactivation_hook( __FILE__, array( $this, 'us_debtclock_widget_deactivation' ) );
@@ -202,24 +222,33 @@ class JCH_Debtclock {
 	public function init() {
 		register_widget( 'Debtclock_Widget' );
 
+		// If the widget is active, enqueue needed CSS
+		if ( is_active_widget( false, false, 'us_debtclock_widget' ) ) {
+			add_action( 'wp_head', array( &$this, 'add_styles_and_scripts' ) );
+		}
+
 	}
 
 	/**
 	 * On activation, schedule an hourly update of the debt data from the source.
 	 */
 	public function us_debtclock_widget_activation() {
-		if ( ! wp_next_scheduled( 'us_debtclock_widget_hourly_event_hook' ) ) {
-			wp_schedule_event( time(), 'hourly', 'us_debtclock_widget_hourly_event_hook' );
+		if ( ! wp_next_scheduled( 'us_debtclock_widget_event_hook' ) ) {
+			wp_schedule_event( time(), 'hourly', 'us_debtclock_widget_event_hook' );
 		}
 
-		add_action( 'us_debtclock_widget_hourly_event_hook', 'get_debt' );
+		add_action( 'us_debtclock_widget_event_hook', 'get_debt' );
 	}
 
 	/**
 	 * On deactivation, remove all functions from the scheduled action hook.
 	 */
 	public function us_debtclock_widget_deactivation() {
-		wp_clear_scheduled_hook( 'us_debtclock_widget_hourly_event_hook' );
+		wp_clear_scheduled_hook( 'us_debtclock_widget_event_hook' );
+	}
+
+	function add_styles_and_scripts() {
+		wp_enqueue_style( 'debtclock_widget_style', plugins_url('style.css', __FILE__) );
 	}
 
 	/**
@@ -234,6 +263,8 @@ class JCH_Debtclock {
 
 		// Query treasury.io - see http://treasury.io/
 		$treasury_api_url = "http://api.treasury.io/cc7znvq/47d80ae900e04f2/sql/?q=";
+
+		// Get the open and close date along with source URL for the total outstanding date, most recent, only one record
 		$debt_sql = 'SELECT "close_today", "open_today", "url" FROM t3c WHERE "item_raw" = \'Total Public Debt Outstanding\' ORDER BY date DESC LIMIT 1';
 		$encoded_debt_sql = urlencode( $debt_sql );
 
@@ -245,15 +276,18 @@ class JCH_Debtclock {
 		 * [{"close_today": 17899001.0, "open_today": 17898403.0, "url": "https://www.fms.treas.gov/fmsweb/viewDTSFiles?fname=14102300.txt&dir=w"}]
 		 */
 
+		// If it was empty, something went wrong
 		if( empty($data) ) return false;
 
 		$us_debtclock_widget_info = reset( json_decode($data, true) ); // Load data into runtime cache
 
+		// If it's a real number, put it in a transient for later use
 		if ( is_numeric( $us_debtclock_widget_info['close_today'] ) ) {
 			set_transient( 'us_debtclock_widget_info', $us_debtclock_widget_info, 1 * 60 * 60 ); // Store in database for up to 1 hour
 		} else {
 			return false;
 		}
+
 		return $us_debtclock_widget_info;
 	}
 
